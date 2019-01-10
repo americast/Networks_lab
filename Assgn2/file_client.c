0efbcff3abd5e6ad2d6a663ddbc77e740df8cf9c
user <user@localhost.localdomain>
2019-01-10 16:43:15 +0530

    Assgn2 update

---
+++ Assgn2/file_client.c
@@ -0,0 +1,85 @@
+
+/*    THE CLIENT PROCESS */
+
+
+#include <stdio.h> 
+#include <fcntl.h> 
+#include <errno.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main()
+{
+	int			sockfd ;
+	struct sockaddr_in	serv_addr;
+
+	int i;
+	char buf[100];
+
+	/* Opening a socket is exactly similar to the server process */
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("Unable to create socket\n");
+		exit(0);
+	}
+
+	/* Recall that we specified INADDR_ANY when we specified the server
+	   address in the server. Since the client can run on a different
+	   machine, we must specify the IP address of the server. 
+
+	   In this program, we assume that the server is running on the
+	   same machine as the client. 127.0.0.1 is a special address
+	   for "localhost" (this machine)
+	   
+	/* IF YOUR SERVER RUNS ON SOME OTHER MACHINE, YOU MUST CHANGE 
+           THE IP ADDRESS SPECIFIED BELOW TO THE IP ADDRESS OF THE 
+           MACHINE WHERE YOU ARE RUNNING THE SERVER. 
+    	*/
+
+	serv_addr.sin_family	= AF_INET;
+	inet_aton("127.0.0.1", &serv_addr.sin_addr);
+	serv_addr.sin_port	= htons(20000);
+
+	/* With the information specified in serv_addr, the connect()
+	   system call establishes a connection with the server process.
+	*/
+	if ((connect(sockfd, (struct sockaddr *) &serv_addr,
+						sizeof(serv_addr))) < 0) {
+		perror("Unable to connect to server\n");
+		exit(0);
+	}
+	else
+		printf("Connection established\n");
+
+	/* After connection, the client can send or receive messages.
+	   However, please note that recv() will block when the
+	   server is not sending and vice versa. Similarly send() will
+	   block when the server is not receiving and vice versa. For
+	   non-blocking modes, refer to the online man pages.
+	*/
+
+	printf("Enter file name: ");
+	gets(buf);
+
+	send(sockfd, buf, strlen(buf) + 1, 0);
+	printf("Sent filename %s from client\n", buf);
+	
+
+    // fcntl(sockfd, F_SETFL, O_NONBLOCK); /* Change the socket into non-blocking state	*/
+
+	memset(buf, 0, 100);
+	printf("Receiving from server\n");
+	int n = recv(sockfd, buf, 100, 0);
+
+	if (n == -1 && errno == ECONNRESET)
+		printf("File not found.\n");
+	// printf("%s\n", buf);
+
+	
+	close(sockfd);
+}
+
+
